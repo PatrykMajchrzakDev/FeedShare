626ef143c6f1aa12a3a31e2c79d7d6d14691a9b1
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2023-04-27 18:32:46 +0200

    Made container for component imports

---
+++ feedshare_frontend/src/components/index.js
@@ -0,0 +1,4 @@
+// Folder collects all components for better visuality when importing
+export { default as Sidebar } from "./Sidebar";
+export { default as Login } from "./Login";
+export { default as UserProfile } from "./UserProfile";
