ea2504cef244c9928ab318810a6dc8d22af99867
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2023-05-08 18:13:17 +0200

    Added Pin rendering and partial functionality - download feature

---
+++ feedshare_frontend/src/components/Pin.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import { MdDownloadForOffline } from "react-icons/md";
+import { AiTwotoneDelete } from "react-icons/ai";
+import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
+import { client, urlFor } from "../client";
+
+//Single pin
+const Pin = ({ pin: { postedBy, image, _id, desination } }) => {
+  const [postHovered, setPostHovered] = useState(false);
+  const [savingPost, setSavingPost] = useState(false);
+  const navigate = useNavigate();
+
+  return (
+    //pin container
+    <div className="m-2">
+      {/* image container */}
+      <div
+        onMouseEnter={() => setPostHovered(true)}
+        onMouseLeave={() => setPostHovered(false)}
+        onClick={() => navigate(`/pin-detail/${_id}`)}
+        className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
+      >
+        <img
+          className="rounded-lg w-full"
+          src={urlFor(image).width(250).url()}
+          alt="user-post"
+        />
+
+        {/* If pin is hovered show these */}
+        {postHovered && (
+          // download image code
+          <div
+            className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
+            style={{ height: "100%" }}
+          >
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2">
+                {/* This allows downloading specific image */}
+                <a
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  //stopPropagation is necessary because otherwise it would trigger all other onClick effects like redirecting to img post
+                  onClick={(e) => e.stopPropagation()}
+                  className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+                >
+                  <MdDownloadForOffline />
+                </a>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Pin;
