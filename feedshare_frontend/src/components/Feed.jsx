2822879bec66325a0fc65f60b6bc74c4c060204a
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2023-05-08 18:18:50 +0200

    Added Spinner on pin load feature

--- feedshare_frontend/src/components/Feed.jsx
+++ feedshare_frontend/src/components/Feed.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+import { feedQuery, searchQuery } from "../utils/data";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  // Spinner state
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
 
-export default Feed
+  // Params to know at what site we are
+  const { categoryID } = useParams();
+
+  //Fetch search query. query is defined in utils/data
+  useEffect(() => {
+    setLoading(true);
+    if (categoryID) {
+      const query = searchQuery(categoryID);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryID]);
+
+  if (loading) return <Spinner message="Adding new images to your feed" />;
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
+};
+
+export default Feed;
