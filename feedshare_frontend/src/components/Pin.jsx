4db10a74b060db7f23668e8ac9d184bea4e3d40e
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2023-05-09 19:26:37 +0200

    Save, delete, show pin url functionalities

--- feedshare_frontend/src/components/Pin.jsx
+++ feedshare_frontend/src/components/Pin.jsx
@@ -5,12 +5,58 @@ import { MdDownloadForOffline } from "react-icons/md";
 import { AiTwotoneDelete } from "react-icons/ai";
 import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
 import { client, urlFor } from "../client";
+import { fetchUser } from "../utils/fetchUser";
 
 //Single pin
-const Pin = ({ pin: { postedBy, image, _id, desination } }) => {
+const Pin = ({ pin }) => {
   const [postHovered, setPostHovered] = useState(false);
   const [savingPost, setSavingPost] = useState(false);
   const navigate = useNavigate();
+
+  const { postedBy, image, _id, destination } = pin;
+
+  //Fetch user info
+  const user = fetchUser();
+
+  // Check if used has saved pin
+  let alreadySaved = pin?.save?.filter(
+    (item) => item?.postedBy?._id === user?.sub
+  );
+
+  //Checks if pin is posted by current logged in user
+  alreadySaved = pin?.save?.filter((item) => item?.postedBy?._id === user?.sub);
+
+  //Delete pin functionality
+  const deletePin = (id) => {
+    client.delete(id).then(() => {
+      window.location.reload();
+    });
+  };
+
+  //Mark pin as favourite functionality
+  const savePin = (id) => {
+    //Checks if pin is already saved
+    if (alreadySaved?.length === 0) {
+      setSavingPost(true);
+      client
+        .patch(id)
+        .setIfMissing({ save: [] })
+        .insert("after", "save[-1]", [
+          {
+            _key: uuidv4(),
+            userId: user.sub,
+            postedBy: {
+              _type: "postedBy",
+              _ref: user.sub,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          setSavingPost(false);
+        });
+    }
+  };
 
   return (
     //pin container
@@ -48,10 +94,73 @@ const Pin = ({ pin: { postedBy, image, _id, desination } }) => {
                   <MdDownloadForOffline />
                 </a>
               </div>
+              {/* If pin is already saved show this state */}
+              {alreadySaved?.length !== 0 ? (
+                <button
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl shadow-md outline-none"
+                >
+                  {pin?.save?.length} Saved
+                </button>
+              ) : (
+                // Show this state if pin has not been saved
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    savePin(_id);
+                  }}
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl shadow-md outline-none"
+                >
+                  {pin?.save?.length} {savingPost ? "Saving" : "Save"}
+                </button>
+              )}
+            </div>
+            {/* Showing pin's url */}
+            <div className="flex justify-between items-center gap-2 w-full">
+              {destination?.slice(8).length > 0 ? (
+                <a
+                  href={destination}
+                  target="_blank"
+                  className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  <BsFillArrowUpRightCircleFill />
+                  {destination?.slice(8, 17)}...
+                </a>
+              ) : undefined}
+
+              {/* Show delete button if creator of post is logged */}
+              {pin?.postedBy?._id === user?.sub && (
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deletePin(_id);
+                  }}
+                  className="bg-white p-2 rounded-full w-8 h-8 flex items-center justify-center text-dark opacity-75 hover:opacity-100 outline-none"
+                >
+                  <AiTwotoneDelete />
+                </button>
+              )}
             </div>
           </div>
         )}
       </div>
+
+      {/* Show link to pin creator underneath pin */}
+      <Link
+        to={`user-profile/${user?._id}`}
+        className="flex gap-2 mt-2 items-center "
+      >
+        <img
+          className="w-8 h-8 rounded-full object-cover"
+          src={postedBy?.image}
+          alt="user-profile"
+        />
+        <p className="font-semibold capitalize">{postedBy?.userName}</p>
+      </Link>
     </div>
   );
 };
